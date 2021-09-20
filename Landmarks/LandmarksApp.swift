54b29bb3d4d1935573df21d1d3cd91e6d6a6f47b
bepyan <65283190+bepyan@users.noreply.github.com>
2021-09-21 03:13:56 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김평안 on 2021/09/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
